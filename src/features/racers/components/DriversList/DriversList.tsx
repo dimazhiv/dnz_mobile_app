f6dcc60b05e142b946e2fa89b1b12032b6e891f4
dzhivoglot <dzhivoglot@wiley.com>
2023-03-03 01:09:06 +0300

    Added DriversList component

---
+++ src/features/racers/components/DriversList/DriversList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import DriverItem from "../DriverItem/DriverItemContainer";
+
+export type DriversListProps = {
+    driversIds: []
+};
+
+const renderItem = ({item}) => <DriverItem driverId={item}/>;
+
+const renderSeparator = () => <View style={styles.separator}/>
+
+
+function DriversList({driversIds}: DriversListProps) {
+    return (
+        <View style={styles.container}>
+            <FlatList
+                ItemSeparatorComponent={renderSeparator}
+                contentContainerStyle={styles.flatList}
+                data={driversIds}
+                renderItem={renderItem}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20
+    },
+    flatList: {
+        flex: 1,
+        paddingTop: 10,
+        paddingBottom: 20
+    },
+    separator: {
+        flex: 1,
+        paddingTop: 20
+    }
+});
+
+export default DriversList;
---
+++ src/features/racers/components/DriversList/DriversListContainer.js
@@ -0,0 +1,20 @@
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {getDriversIds} from "../../selectors";
+import DriversList from "./DriversList";
+
+function mapStateToProps(state) {
+    return {
+        driversIds: getDriversIds(state)
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(
+        {},
+        dispatch
+    );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DriversList);
+
