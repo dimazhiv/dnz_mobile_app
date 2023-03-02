ea8283894fe9c3c698a6d1648f23dbae5b70c2a3
dzhivoglot <dzhivoglot@wiley.com>
2023-03-03 01:06:05 +0300

    Update API to get drivers with offset and limit

--- src/features/services/api.ts
+++ src/features/services/api.ts
@@ -1,10 +1,20 @@
 import axios from "axios";
 
-export async function fetchDriversData() {
+async function fetchDriversData(limit: number, offset: number) {
     try {
-        const response = await axios.get("https://ergast.com/api/f1/drivers.json")
-        return response.data.MRData.DriverTable;
+        const response = await axios.get(`https://ergast.com/api/f1/drivers.json?limit=${limit}&offset=${offset}`)
+        return response.data.MRData.DriverTable.Drivers;
     } catch (error) {
         console.error(error);
     }
+}
+
+export async function loadDriversData(limit: number, offset: number) {
+    const drivers = await fetchDriversData(limit, offset);
+    const driversIds = drivers ? drivers.map((item) => item.driverId) : [];
+    const byId = drivers.reduce((acc, item) => {
+        acc[item.driverId] = item;
+        return acc;
+    }, {})
+    return {byId, driversIds}
 }
