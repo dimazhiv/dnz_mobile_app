1a20c4717e6581a1a31d8f10c4acc912a0d59eac
dzhivoglot <dzhivoglot@wiley.com>
2023-03-06 21:01:37 +0300

    Updated reducers and state for pagination

--- src/features/racers/slice.ts
+++ src/features/racers/slice.ts
@@ -1,32 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../setupStore';
 
 interface RacersState {
   driversIds: [];
   byId: {};
   activeDriverInfo: {};
+  currentPageNumber: number;
+  maxOffset: number;
 }
 
 const initialState: RacersState = {
   driversIds: [],
   byId: {},
-  activeDriverInfo: {}
+  activeDriverInfo: {},
+  currentPageNumber: 1,
+  maxOffset: 0
 };
 
 const slice = createSlice({
   name: 'racers',
   initialState,
   reducers: {
-    setDriversData(state, action) {
+    setDriversData: (state: RootState, action) => {
       const { byId, driversIds } = action.payload;
-      state.driversIds = driversIds;
-      state.byId = byId;
+      state.driversIds.push(...driversIds);
+      Object.assign(state.byId, { ...byId });
     },
-    setActiveDriverInfo(state, action) {
-      const { driverInfo } = action.payload;
-      state.activeDriverInfo = driverInfo;
+    setActiveDriverInfo: (state: RootState, action) => {
+      state.activeDriverInfo = action.payload;
+    },
+    setCurrentPage: (state: RootState, action) => {
+      state.currentPageNumber = action.payload;
+    },
+    setMaxOffset: (state: RootState, action) => {
+      state.maxOffset = action.payload;
     }
   }
 });
 
-export const { setDriversData, setActiveDriverInfo } = slice.actions;
+export const { setDriversData, setActiveDriverInfo, setCurrentPage, setMaxOffset } = slice.actions;
 export default slice.reducer;
