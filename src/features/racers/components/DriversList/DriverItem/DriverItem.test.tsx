53e6f558c305dd90514fc1f40f36f8dbdb3f0b04
dzhivoglot <dzhivoglot@wiley.com>
2023-03-11 20:09:25 +0300

    Experiment with renderer for tests

--- src/features/racers/components/DriversList/DriverItem/DriverItem.test.tsx
+++ src/features/racers/components/DriversList/DriverItem/DriverItem.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import DriverItem from './DriverItem';
+import renderer from 'react-test-renderer';
+import {shallow} from "enzyme";
 
 describe('Snapshot testing DriverItem component', () => {
   test('correct component view', () => {
@@ -15,5 +16,30 @@ describe('Snapshot testing DriverItem component', () => {
       />
     );
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('qwe', () => {
+  it('updates number when clicked', () => {
+    const inst = renderer.create(
+      <DriverItem
+        dateOfBirth={'01.09.1994'}
+        fullName={'Carlos Sainz'}
+        nationality={'Spanish'}
+        driverId={'sainz'}
+        onPress={() => {}}
+        onRacesPress={() => {}}
+      />
+    );
+    // const button = inst.root.findByType(TouchableOpacity);
+    // const text = inst.root.findAllByType(TouchableOpacity);
+    // console.log('!!!!!!!!CONSOLE!!!!!!!!: ', text);
+    // default state
+    // expect(text.props.children).toBe(1);
+    // or
+    // expect(inst.root.instance.state.number).toBe(1);
+    // text[1].props.onPress(); // state was updated by the button press
+    // expect(text[0].props.onPress).toBeCalled();
+    // expect(inst.root.props.onRacesPress).toBeCalledWith('sainz');
   });
 });
