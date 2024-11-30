4a7ad1be91a5c4efb5c4c612c1be3e28dfe648b1
Dmitriy Zhivoglotov <dzhivoglotov@samokat.ru>
2024-11-30 20:25:13 +0300

    Update rn to 76 temp commit

---
+++ android/app/src/main/java/com/dnz_mobile_app/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dnz_mobile_app
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import android.os.Bundle;
+
+class MainActivity : ReactActivity() {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "dnz_mobile_app"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
